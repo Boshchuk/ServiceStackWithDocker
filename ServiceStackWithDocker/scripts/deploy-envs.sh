f400486b121939cf3de801bdeb4ca341797f21bc
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2019-07-03 22:45:39 +0300

    deploy-envs fixed

--- ServiceStackWithDocker/scripts/deploy-envs.sh
+++ ServiceStackWithDocker/scripts/deploy-envs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=netcoreapps-rockwind-azure
+export IMAGE_NAME=servicestack-with-docker-azure
 export IMAGE_VERSION=latest
 
-export AZURE_REGISTRY=netcoreapps.azurecr.io
+export AZURE_REGISTRY=serivcestack.azurecr.io
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AZURE_REGISTRY_LOGIN, AZURE_REGISTRY_PASSWORD
